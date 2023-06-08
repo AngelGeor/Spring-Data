b229b49d9330cd3570454e42d4bd858922074f18
Angel Georgiev <angelgeor0560@gmail.com>
2023-06-08 16:25:20 +0300

    Add files via upload

---
+++ Hibernate - Code First/HibernateCodeFirstLab/src/main/java/Entities/Bike.java
@@ -0,0 +1,15 @@
+package Entities;
+
+import javax.persistence.Entity;
+import javax.persistence.Table;
+
+@Entity
+@Table(name = "bikes")
+public class Bike extends Vehicle{
+    private static final String BIKE_TYPE = "BIKE";
+
+    public Bike() {
+        super(BIKE_TYPE);
+    }
+
+}
